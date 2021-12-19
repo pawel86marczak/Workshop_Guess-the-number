8a33630b540a4a6d93bdfd49f4db50639500d826
pawel <pawel86marczak@gmail.com>
2021-12-19 08:40:47 +0100

    describtion

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,5 +1,17 @@
 import random
 
+'''
+1. Ask the question "Guess the number:" and retrieve the number from the keyboard.
+2. Check if the entered text is really a number and in the event of an error display the message "It's not a number!", 
+Then go back to pt. 1
+3. If the number entered by the user is less than the random number, display the message "To small!", 
+Then return to pt. 1.
+4. If the number given by the user is greater than the randomly selected number, display the message "To big!", 
+Then go back to pt. 1.
+5. If the number entered by the user is equal to the random number, 
+display the message "You win!" And exit the program. 
+'''
+
 number = random.randrange(1, 2)
 try:
     guess = int(input("Guess the number :"))
@@ -14,7 +26,6 @@ try:
 except ValueError:
     print("It's not a number!")
     guess = int(input("Guess the number :"))
-
 
 if guess == number:
     print("You win")
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Workshop_Guess-the-number
 simple number guessing game
+
+This game will:
+
+1. Ask the question "Guess the number:" and retrieve the number from the keyboard.
+2. Check if the entered text is really a number and in the event of an error display the message "It's not a number!", 
+Then go back to pt. 1
+3. If the number entered by the user is less than the random number, display the message "To small!", 
+Then return to pt. 1.
+4. If the number given by the user is greater than the randomly selected number, display the message "To big!", 
+Then go back to pt. 1.
+5. If the number entered by the user is equal to the random number, 
+display the message "You win!" And exit the program. 
+
+To play the game open the file "Guess_the_number.py". You can do it in your terminal with command 'python3 Guess_the_number.py'
